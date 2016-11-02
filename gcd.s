bafc021d870a7edd444a80a8bf4a3c007aabe787
SamChatfield <samchatfield97@gmail.com>
2016-11-02 20:23:27 +0000

    I/O working

--- gcd.s
+++ gcd.s
@@ -3,21 +3,30 @@
     input1: .asciiz "\nEnter first number: "
     input2: .asciiz "\nEnter second number: "
     cr: .asciiz "\n"
-    errmsg: .asciiz "\nError - must enter a positive integer\n"
 
 .text
 .globl main
     main:
-        INPUT:
-            li $v0,4 # ask for first number
-                la $a0,input1
-                syscall
-                li $v0,5
-                syscall
-                move $a0,$v0
-            li $v0,4 # ask for second number
-                la $a1,input2
-                syscall
-                li $v0,5
-                syscall
-                move $a1,$v0
+        # Ask for first number
+        li $v0,4
+        la $a0,input1
+        syscall
+        # Read the first number
+        li $v0,5
+        syscall
+        move $s0,$v0
+        # Ask for second number
+        li $v0,4
+        la $a0,input2
+        syscall
+        # Read the second number
+        li $v0,5
+        syscall
+        move $s1,$v0
+        # j EXIT
+    GCD:
+        # Compute GCD of two numbers
+        
+    EXIT:
+        li $v0,10
+        syscall
